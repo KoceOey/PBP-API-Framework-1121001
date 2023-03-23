5ccfdff35bb0ad3410ed0792d2484536029e553a
koceoey <nicho2505.nc@gmail.com>
2023-03-23 23:46:50 +0700

    reset

--- main.go
+++ main.go
@@ -14,6 +14,6 @@ func main() {
 	router.POST("/movies", controllers.InsertMovies)
 	router.PUT("/movies/:id", controllers.UpdateMovies)
 	router.DELETE("/movies/:id", controllers.DeleteMovies)
-	router.Run("localhost:8080")test
+	router.Run("localhost:8080")
 	fmt.Println("Serving localhost:8080")
 }
